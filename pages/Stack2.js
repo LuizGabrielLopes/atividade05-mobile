f56a2e57d712cfeba81915288055ad36510908d1
Luiz Gabriel <luiz.gabriellopesc@gmail.com>
2025-04-01 14:09:37 -0300

    feat: activity 5

---
+++ pages/Stack2.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Stack2() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Página 2</Text>
+    </View>
+  );
+}
+
+const styles = {
+  conatiner: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+};
